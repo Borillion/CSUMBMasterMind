589a3a3a790c6e70c2f133507bbb861ff93a3324
Borillion <borillion_star@yahoo.com>
2015-02-01 13:26:08 -0800

    Update asign_4_2.php

--- asign_4_2.php
+++ asign_4_2.php
@@ -4,36 +4,42 @@ require '../db_connection.php';
 function db_count() {
 	global $dbConn;
 	
-	if(isset($_GET['term'])){
-		if(isset($_GET['DIRECTION'])){
+	sql = "SELECT * FROM movies"
+	
+	if(isset($_GET['movie_id'])){
+		return;
+	}
 			
+ if(isset($_GET['term'])){
+		if(isset($_GET['DIRECTION'])){
 			$term = $_GET['term'];
 			$direction = $_GET['DIRECTION'];
 			
-			if(strcmp($term,"movie_title")){
-				if($direction == 1){
-					$sql = "SELECT * FROM movies ORDER BY movie_title ASC";
-				}else{
-					$sql = "SELECT * FROM movies ORDER BY movie_title DESC";		
-				}
-			}elseif(strcmp($term,"year")){
-				if($direction == 1){
-					$sql = "SELECT * FROM movies ORDER BY year ASC";
-				}else{
-					$sql = "SELECT * FROM movies ORDER BY year DESC";
-				}
-			}
-		}
-		
-		
+			$sql .= " ORDER BY field = :field DESC";
+			
+	}else{
+		$sql = "SELECT * FROM movies";
+	}
+	
+	
 	$stmt = $dbConn -> prepare($sql);
 	$stmt -> execute();
 	return $stmt->fetchAll();
+	
 	}
-}
+	
 	
 
-
+	function showMovie($movie_id){
+		global $dbConn;
+		
+		$sql = "SELECT * FROM movies WHERE movie_id = :movie_id";
+		$stmt = $dbConn -> prepare($sql);
+		$stmt -> execute (array (':movie_id' => $movie_id));
+		$movie_array = $stmt->fetch();
+		
+		echo "<p>" . $movie_array['movie_title'] . " " .  $movie_array['year'] . "</p>";
+	}
 ?>
 
 
@@ -48,7 +54,7 @@ function db_count() {
 
 <title>HTML</title>
 <meta name="description" content="">
-<meta name="author" content="babak" >
+<meta name="author" content="babak">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -78,25 +84,57 @@ function db_count() {
 		</select> <input type="submit" value="Go!" />
 	</form>
 
-	<table>
-		<th></th>
-		<tr>Movie Title
-		</tr>
-		<tr>Year
-		</tr>
-		<tr>Director
-		</tr>
-		<th></th>	
 	
 	<?php 
 		$list = db_count();
-		foreach($list as $movie){
-		   echo "<tr><td>" .  $movie[0];
-		   echo "</td><td>" . $movie[1];
+		if(isset($_GET['movie_id'])){
+			$movie_id = $_GET['movie_id'];
+			showMovie($movie_id);
+		}
+		if(!empty($list)){
+			echo "<table>
+			<th></th>
+			<tr>Movie Title
+			</tr>
+			<tr>Year
+			</tr>
+			<tr>Director
+			</tr>
+			<th></th>";
+		foreach($list as $movie){	
+			// href="?inav=search&var=year&var1='.$results['year'].'">'.$results['year'].'</a>
+		   echo '<tr><td><a class="movie" href="?movie_id='.$movie[0].'">'.$movie[1].'</a></td></tr>';
 		   echo "</td><td>" . $movie[2];
-		   echo "</td></tr>";
+		   echo "</td></tr>"; 
 		} 
+		echo "</table>";
+	}
 	?>
-	</table>
+
 </body>
 </html>
+<?php 
+	/* if(isset($_GET['term'])){
+		if(isset($_GET['DIRECTION'])){
+			
+			$term = $_GET['term'];
+			$direction = $_GET['DIRECTION'];
+			
+			if(strcmp($term,"movie_title")){
+				if($direction == 1){
+					$sql = "SELECT * FROM movies ORDER BY movie_title ASC";
+				}else{
+					$sql = "SELECT * FROM movies ORDER BY movie_title DESC";		
+				}
+			}elseif(strcmp($term,"year")){
+				if($direction == 1){
+					$sql = "SELECT * FROM movies ORDER BY year ASC";
+				}else{
+					$sql = "SELECT * FROM movies ORDER BY year DESC";
+				}
+			}
+		}
+	}else{
+		$sql = "SELECT * FROM movies";
+ 	} */
+ 	?>
